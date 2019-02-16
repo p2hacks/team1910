ffc8f5fbdd90fb5433ca795bde78a7591a19568f
西俣雷斗 <raitonishimata@nishimataraitonoMacBook-Air.local>
2019-02-16 17:58:58 +0900

    PodsとDS_Storeを削除

--- Pods/Target Support Files/Pods-MotokApp/Pods-MotokApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MotokApp : NSObject
-@end
-@implementation PodsDummy_Pods_MotokApp
-@end
--- Pods/Target Support Files/Pods-MotokAppUITests/Pods-MotokAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MotokAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MotokAppUITests
-@end
